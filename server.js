ecd21ccb918107af938f1450d93a2f7b0fd7a116
KAUSHIK-NITHI <kaushik.nithi@gmail.com>
2025-02-24 14:48:14 +0530

    updated

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const data = express();
+const app = server(3000);
+const use = app.json();
+
+Myuser={};
+
+const user = [
+    {username: "alice" , age: 25 , email: "alice@gmail.com" },
+    { username: "bob" , age:30, email: "bob@gmail.com"},
+    {username: "charlie", age:28, email: "charlie@gmail.com"},
+];
